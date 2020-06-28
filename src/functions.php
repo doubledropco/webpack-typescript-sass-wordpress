d849c316e0463b371a893cad30728327c44ae279
Jon Sakas <jon.sakas@gmail.com>
2020-06-28 17:33:58 -0600

    Display page template name in pages tables
    Closes #4

--- src/functions.php
+++ src/functions.php
@@ -128,6 +128,35 @@ function register_scripts()
 add_action('wp_enqueue_scripts', 'register_scripts');
 
 /**
+ * Display page template name in pages tables
+ */
+add_filter('manage_pages_columns', 'page_column_views');
+add_action('manage_pages_custom_column', 'page_custom_column_views', 5, 2);
+function page_column_views($defaults)
+{
+    $defaults['page-layout'] = __('Template');
+    return $defaults;
+}
+
+function page_custom_column_views($column_name, $id)
+{
+    if ($column_name === 'page-layout') {
+        $set_template = get_post_meta(get_the_ID(), '_wp_page_template', true);
+        if ($set_template == 'default') {
+            echo 'Default';
+        }
+        $templates = get_page_templates();
+        ksort($templates);
+        foreach (array_keys($templates) as $template):
+            if ($set_template == $templates[$template]) {
+                echo $template;
+            }
+
+        endforeach;
+    }
+}
+
+/**
  * Register navigation menus uses wp_nav_menu in five places.
  */
 function menus()
