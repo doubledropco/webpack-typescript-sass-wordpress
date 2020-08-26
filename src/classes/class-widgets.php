9a0006426b88453635c7caf55e8019b2fee3a270
Jon Sakas <jon.sakas@gmail.com>
2020-08-26 12:14:35 -0600

    Fix lint

--- src/classes/class-widgets.php
+++ src/classes/class-widgets.php
@@ -1,79 +1,93 @@
 <?php
 
-class Widgets
-{
-    /**
-     * Initialization
-     * This method should be run from functions.php
-     */
-    public function __construct()
-    {
-        add_action('widgets_init', [__CLASS__, 'register_widgets']);
-}
+class Widgets {
 
-    /**
-     * Register Widgets For Wordpress
-     */
-    public static function register_widgets()
-    {
-        if (function_exists('register_sidebar')):
+	/**
+	 * Initialization
+	 * This method should be run from functions.php
+	 */
+	public function __construct() {
+		add_action( 'widgets_init', array( __CLASS__, 'register_widgets' ) );
+	}
 
-            register_sidebar(array(
-                'name' => 'Header Navigation',
-                'id' => 'widget-header-navigation',
-                'before_widget' => '<div class="header__widget-header-navigation d-none d-md-block">',
-                'after_widget' => '</div>',
-            ));
+	/**
+	 * Register Widgets For WordPress
+	 */
+	public static function register_widgets() {
+		if ( function_exists( 'register_sidebar' ) ) :
 
-            register_sidebar(array(
-                'name' => 'Mobile Menu (Before)',
-                'id' => 'widget-mobile-menu-before',
-                'before_widget' => '<div class="header__mobile-nav-widget-before">',
-                'after_widget' => '</div>',
-            ));
+			register_sidebar(
+				array(
+					'name'          => 'Header Navigation',
+					'id'            => 'widget-header-navigation',
+					'before_widget' => '<div class="header__widget-header-navigation d-none d-md-block">',
+					'after_widget'  => '</div>',
+				)
+			);
 
-            register_sidebar(array(
-                'name' => 'Mobile Menu (After)',
-                'id' => 'widget-mobile-menu-after',
-                'before_widget' => '<div class="header__mobile-nav-widget-after">',
-                'after_widget' => '</div>',
-            ));
+			register_sidebar(
+				array(
+					'name'          => 'Mobile Menu (Before)',
+					'id'            => 'widget-mobile-menu-before',
+					'before_widget' => '<div class="header__mobile-nav-widget-before">',
+					'after_widget'  => '</div>',
+				)
+			);
 
-            register_sidebar(array(
-                'name' => 'Footer Column 1',
-                'id' => 'widget-footer-1',
-                'before_widget' => '<div class="footer__widget-1">',
-                'after_widget' => '</div>',
-            ));
+			register_sidebar(
+				array(
+					'name'          => 'Mobile Menu (After)',
+					'id'            => 'widget-mobile-menu-after',
+					'before_widget' => '<div class="header__mobile-nav-widget-after">',
+					'after_widget'  => '</div>',
+				)
+			);
 
-            register_sidebar(array(
-                'name' => 'Footer Column 2',
-                'id' => 'widget-footer-2',
-                'before_widget' => '<div class="footer__widget-2">',
-                'after_widget' => '</div>',
-            ));
+			register_sidebar(
+				array(
+					'name'          => 'Footer Column 1',
+					'id'            => 'widget-footer-1',
+					'before_widget' => '<div class="footer__widget-1">',
+					'after_widget'  => '</div>',
+				)
+			);
 
-            register_sidebar(array(
-                'name' => 'Footer Column 3',
-                'id' => 'widget-footer-3',
-                'before_widget' => '<div class="footer__widget-3">',
-                'after_widget' => '</div>',
-            ));
+			register_sidebar(
+				array(
+					'name'          => 'Footer Column 2',
+					'id'            => 'widget-footer-2',
+					'before_widget' => '<div class="footer__widget-2">',
+					'after_widget'  => '</div>',
+				)
+			);
 
-            register_sidebar(array(
-                'name' => 'Footer Column 4',
-                'id' => 'widget-footer-4',
-                'before_widget' => '<div class="footer__widget-4">',
-                'after_widget' => '</div>',
-            ));
+			register_sidebar(
+				array(
+					'name'          => 'Footer Column 3',
+					'id'            => 'widget-footer-3',
+					'before_widget' => '<div class="footer__widget-3">',
+					'after_widget'  => '</div>',
+				)
+			);
 
-            register_sidebar(array(
-                'name' => 'Footer Disclaimer Section',
-                'id' => 'widget-footer-5',
-                'before_widget' => '<div class="footer__widget-5">',
-                'after_widget' => '</div>',
-            ));
+			register_sidebar(
+				array(
+					'name'          => 'Footer Column 4',
+					'id'            => 'widget-footer-4',
+					'before_widget' => '<div class="footer__widget-4">',
+					'after_widget'  => '</div>',
+				)
+			);
 
-        endif;
-    }
+			register_sidebar(
+				array(
+					'name'          => 'Footer Disclaimer Section',
+					'id'            => 'widget-footer-5',
+					'before_widget' => '<div class="footer__widget-5">',
+					'after_widget'  => '</div>',
+				)
+			);
+
+		endif;
+	}
 }
--- src/template-parts/blocks/hero/hero.php
+++ src/template-parts/blocks/hero/hero.php
@@ -10,63 +10,67 @@
  */
 
 
-$slides = get_field('slides');
-$background_image = $is_preview ? '' : get_field('background_image');
-$background_overlay = $is_preview ? '' : get_field('background_overlay');
+$slides             = get_field( 'slides' );
+$background_image   = $is_preview ? '' : get_field( 'background_image' );
+$background_overlay = $is_preview ? '' : get_field( 'background_overlay' );
 
-if ($background_image) {
-    $background_image_src = wp_get_attachment_image_src($background_image, 'fullscreen')[0];
+if ( $background_image ) {
+	$background_image_src = wp_get_attachment_image_src( $background_image, 'fullscreen' )[0];
 }
 
 $id = 'hero-' . $block['id'];
-if( !empty($block['anchor']) ) {
-    $id = $block['anchor'];
+if ( ! empty( $block['anchor'] ) ) {
+	$id = $block['anchor'];
 }
 
-$className = 'hero';
-if( !empty($block['className']) ) {
-    $className .= ' ' . $block['className'];
+$class_name = 'hero';
+if ( ! empty( $block['className'] ) ) {
+	$class_name .= ' ' . $block['className'];
 }
-if( !empty($block['align']) ) {
-    $className .= ' align' . $block['align'];
+if ( ! empty( $block['align'] ) ) {
+	$class_name .= ' align' . $block['align'];
 }
 
 ?>
-<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-    <div class="hero__bg parallax" data-bg="<?php echo $background_image_src; ?>"></div>
-    <?php if ($background_overlay): ?>
-        <div class="hero__overlay"></div>
-    <?php endif; ?>
-    <div class="hero__carousel">
-        <?php 
-            if ($slides): foreach ($slides as $slide): 
-                $headline = $slide['headline']; 
-                $copy = $slide['copy']; 
-                $cta = $slide['cta']; 
-        ?>
-            <div class="hero__carousel-cell">
-                <div class="hero__slide">
-                    <div class="hero__slide-content">
-                        <div class="hero__slide-left">
-                            <h2 class="sm-element sm-element--d1"><?php echo $headline; ?></h2>
-                        </div>
-                        <div class="hero__slide-right">
-                            <div class="hero__copy sm-element sm-element--d2">
-                                <?php if ($copy): ?>
-                                    <?php echo $copy; ?>
-                                <?php endif; ?>
-                            </div>
+<div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
+	<div class="hero__bg parallax" data-bg="<?php echo $background_image_src; ?>"></div>
+	<?php if ( $background_overlay ) : ?>
+		<div class="hero__overlay"></div>
+	<?php endif; ?>
+	<div class="hero__carousel">
+		<?php
+		if ( $slides ) :
+			foreach ( $slides as $slide ) :
+				$headline = $slide['headline'];
+				$copy     = $slide['copy'];
+				$cta      = $slide['cta'];
+				?>
+			<div class="hero__carousel-cell">
+				<div class="hero__slide">
+					<div class="hero__slide-content">
+						<div class="hero__slide-left">
+							<h2 class="sm-element sm-element--d1"><?php echo $headline; ?></h2>
+						</div>
+						<div class="hero__slide-right">
+							<div class="hero__copy sm-element sm-element--d2">
+							<?php if ( $copy ) : ?>
+									<?php echo $copy; ?>
+								<?php endif; ?>
+							</div>
 
-                            <?php if ($cta): ?>
-                                <div class="hero__cta-wrap sm-element sm-element--d3">
-                                    <?php button($cta, 'btn-primary'); ?>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; endif; ?>
-    </div>
-    
+						<?php if ( $cta ) : ?>
+								<div class="hero__cta-wrap sm-element sm-element--d3">
+									<?php button( $cta, 'btn-primary' ); ?>
+								</div>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+					<?php
+		endforeach;
+endif;
+		?>
+	</div>
+
 </div>
