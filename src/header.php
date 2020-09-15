8df006131e47d10f90488777684827b9e5c8ea42
Jon Sakas <jon.sakas@gmail.com>
2020-09-15 13:20:37 -0600

    Fix support for GTM and FB snippet fields
    Closes #34
    Closes #33

--- src/header.php
+++ src/header.php
@@ -16,6 +16,7 @@ $site_name              = get_bloginfo( 'name' );
 $site_url               = get_site_url();
 $logo                   = get_field( 'logo', 'option' );
 $google_tag_manager_key = get_field( 'google_tag_manager_key', 'option' );
+$facebook_app_id        = get_field( 'facebook_app_id', 'option' );
 $google_maps_api_key    = get_field( 'google_maps_api_key', 'option' );
 $header_menu_animation  = get_field( 'header_menu_animation', 'option' );
 
@@ -46,6 +47,26 @@ endif;
 			<script>
 				window.gmak = '<?php echo $google_maps_api_key; ?>';
 			</script>
+		<?php endif; ?>
+
+		<?php if ( $facebook_app_id ) : ?>
+			<!-- Facebook Pixel Code -->
+			<script>
+			!function(f,b,e,v,n,t,s)
+			{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+			n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t,s)}(window, document,'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '<?php echo $facebook_app_id; ?>');
+			fbq('track', 'PageView');
+			</script>
+			<noscript><img height="1" width="1" style="display:none"
+			src="https://www.facebook.com/tr?id=<?php echo $facebook_app_id; ?>&ev=PageView&noscript=1"
+			/></noscript>
+			<!-- End Facebook Pixel Code -->
 		<?php endif; ?>
 
 		<?php wp_head(); ?>
